64b10f468cce45065840781d898e57ddcb3147f6
Dmitriy Kiyatkin <d.kiyatkin@itlf.ru>
2014-03-26 13:51:08 +0400

    bug fix

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
+var fs = require('fs');
 var path = require('path');
-var infrajs = require('infrajs');
-var reg_njs = new RegExp(infrajs.regExpStr_njs, 'i');
+var url = require('url');
+var regNjs = new RegExp('(node(_|-)[^/]*/.*\\.)|(\\.n\\.{0,1})js$', 'i');
+var regNcoffee = new RegExp('.+\\.n\\.coffee$', 'i');
+require('coffee-script/register');
 
 module.exports = function(options) {
-	var root = options.root;
+	var root = options.publicDir;
 	return function(req, res, next) {
-		if (reg_njs.test(req.url_parse.pathname)) {
-			var pathname = decodeURIComponent(path.join('/',req.url_parse.pathname));
+		var urlpathname = url.parse(req.url).pathname;
+		if (regNjs.test(urlpathname) || regNcoffee.test(urlpathname)) {
+			var pathname = decodeURIComponent(path.join('/', urlpathname));
 			var js = path.join(root, pathname);
-			path.exists(js, function(exists) {
+			fs.exists(js, function(exists) {
 				if (exists) {
 					try {
 						require(js).init(req, res, next, root);
 					} catch(e) {
-						console.log('error', e);
-						res.writeHead(502); res.end('Bad Gateway');
+						console.error('error', e);
+						res.writeHead(502);
+						res.end('Bad Gateway');
 					}
 				} else {
-					res.writeHead(404); res.end('Not Found');
+					res.writeHead(404);
+					res.end('Not Found');
 				}
 			});
 		} else { next(); }
